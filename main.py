0e8b7d1b75e218092a5a5331a2a08cf7e60ab9af
ellidate92 <ellidate92@gmail.com>
2023-04-16 21:02:35 -0500

    translate messages

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ if __name__ == "__main__":
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
     else:
-        print("Se debe indicar el fichero como primer argumento")
-        print("El segundo argumento indica si se quieren eliminar duplicados")
+        print("The file must be specified as the first argument") #= print("Se debe indicar el fichero como primer argumento")
+        print("The second argument indicates whether you want to remove duplicates") #= print("El segundo argumento indica si se quieren eliminar duplicados")
         sys.exit(1)
 
-    print(f"Se leerán las palabras del fichero {filename}")
+    print(f"The words of the file {filename} will be read.") #= print(f"Se leerán las palabras del fichero {filename}")
     file_path = os.path.join(".", filename)
     if os.path.isfile(file_path):
         word_list = []
@@ -40,7 +40,7 @@ if __name__ == "__main__":
             for line in file:
                 word_list.append(line.strip())
     else:
-        print(f"El fichero {filename} no existe")
+        print(f"The file {filename} does not exist") #= print(f"El fichero {filename} no existe")
         word_list = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"]
 
     if remove_duplicates:
