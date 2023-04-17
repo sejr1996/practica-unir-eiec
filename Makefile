99c463bbf1680ff829790589cd015018d4d1548e
Diego  Maigualca <diego.maigualca@nagarro.com>
2023-04-16 20:34:15 -0500

    Reformat Readme.md & Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,18 @@
-.PHONY: all $(MAKECMDGOALS)
+IMAGE_NAME := python:3.6-slim
+ARGS := words.txt yes
 
-run:
-	docker run --rm --volume `pwd`:/opt/app --env PYTHON_PATH=/opt/app -w /opt/app python:3.6-slim python3 main.py words.txt yes
+.PHONY: all run
+
+all: run
+
+run: main.py
+	docker run --rm \
+		--volume `pwd`:/opt/app \
+		--env PYTHON_PATH=/opt/app \
+		-w /opt/app \
+		$(IMAGE_NAME) \
+		python3 main.py $(ARGS)
+
+# Comentarios
+# main.py es la entrada del programa
+# La regla run depende de main.py
--- main.py
+++ main.py
@@ -6,19 +6,38 @@ Organization: UNIR
 import os
 import sys
 
+
 DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
+
+
+def read_words_from_file(file_path):
+    words_set = set()
+    with open(file_path, "r") as file:
+        for line in file:
+            word = line.strip()
+            if word:
+                words_set.add(word)
+    return sorted(words_set)
+
+
+def get_word_list(filename=DEFAULT_FILENAME):
+    file_path = os.path.join(".", filename)
+    if os.path.isfile(file_path):
+        return read_words_from_file(file_path)
+    else:
+        print(f"El fichero {filename} no existe")
+        return ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"]
+
+
+def remove_duplicates_from_list(items):
+    return list(set(items))
 
 
 def sort_list(items, ascending=True):
     if not isinstance(items, list):
         raise RuntimeError(f"No puede ordenar {type(items)}")
-
     return sorted(items, reverse=(not ascending))
-
-
-def remove_duplicates_from_list(items):
-    return list(set(items))
 
 
 if __name__ == "__main__":
@@ -33,16 +52,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
-    file_path = os.path.join(".", filename)
-    if os.path.isfile(file_path):
-        word_list = []
-        with open(file_path, "r") as file:
-            for line in file:
-                word_list.append(line.strip())
-    else:
-        print(f"El fichero {filename} no existe")
-        word_list = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"]
-
+    word_list = get_word_list(filename)
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
